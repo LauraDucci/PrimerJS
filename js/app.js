e70f47149495e31e0b1b2436fbdb779b316196fa
Laura Berarducci <mlauraberarducci@gmail.com>
2021-06-01 23:43:11 -0300

    Iniciando Java Script 3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-alert("Hola Mundo");
+let nombre = "Laura";
+console.log(nombre);
+// alert ("Hola Mundo"); muestra un cartel de alerta.
+document.write ("<h1>Hola Mundo</h1>");
+
+// // pedir datos al usuario.
+// let nombreUsuario = prompt("Como es tu nombre?");
+// let nombreUsuario2 = prompt("Como se llama tu amigo?");
+
+// //Esto es concatenar con la coma "," o el "+"
+// document.write ("Hola ", nombreUsuario, " y ", nombreUsuario2);
+
+//variables numericas
+let edad = prompt ("Cual es tu edad?");
+console.log (edad);
